ece97fc28a8b0010c58ccdb019d767b3d0ef1bf1
dianaandreea03 <68383235+dianaandreea03@users.noreply.github.com>
2020-08-05 19:06:31 +0300

    healthbar, manabar updated

--- rpgDoomsday/Assets/Scripts/UIHUD.cs
+++ rpgDoomsday/Assets/Scripts/UIHUD.cs
@@ -13,10 +13,15 @@ public class UIHUD : MonoBehaviour
     public TMP_Text lvlText;
     public TMP_Text goldText;
 
+    public Slider hslider;
+    public Slider mslider;
+
     private void Update()
     {
         healthText.text = Mathf.FloorToInt(hero.health) + "/" + Mathf.FloorToInt(hero.healthMax);
+        hslider.value = hero.health;
         manaText.text = Mathf.FloorToInt(hero.mana) + "/" + Mathf.FloorToInt(hero.manaMax);
+        mslider.value = hero.mana;
         xpText.text = hero.xp + "/" + hero.ExperienceForLevel(hero.level);
         lvlText.text = "Level " + hero.level;
         goldText.text = hero.inventory.gold + " Gold";
