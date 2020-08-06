ec5de1d5a08574d419cfcc530e2d6c2e4fc0f8cb
Alexgaby15 <r_alexgaby@yahoo.com>
2020-08-06 11:40:59 +0300

    Added STR, DEX, WIS, Unspent points and Class to UI. Fixed Level, Health and Mana displays.

--- rpgDoomsday/Assets/Scripts/UIHUD.cs
+++ rpgDoomsday/Assets/Scripts/UIHUD.cs
@@ -12,19 +12,35 @@ public class UIHUD : MonoBehaviour
     public TMP_Text xpText;
     public TMP_Text lvlText;
     public TMP_Text goldText;
+    public TMP_Text strText;
+    public TMP_Text dexText;
+    public TMP_Text wisText;
+    public TMP_Text pointsText;
 
     public Slider hslider;
     public Slider mslider;
+    public Slider xpslider;
 
     private void Update()
     {
         healthText.text = Mathf.FloorToInt(hero.health) + "/" + Mathf.FloorToInt(hero.healthMax);
-        hslider.value = hero.health;
+        hslider.value = Mathf.FloorToInt(hero.health);
+        hslider.maxValue = Mathf.FloorToInt(hero.healthMax);
         manaText.text = Mathf.FloorToInt(hero.mana) + "/" + Mathf.FloorToInt(hero.manaMax);
-        mslider.value = hero.mana;
+        mslider.value = Mathf.FloorToInt(hero.mana);
+        mslider.maxValue = Mathf.FloorToInt(hero.manaMax);
         xpText.text = hero.xp + "/" + hero.ExperienceForLevel(hero.level);
+        xpslider.value = hero.xp;
+        xpslider.maxValue = hero.ExperienceForLevel(hero.level);
         lvlText.text = "Level " + hero.level + " " + GetHeroClassName(hero.heroClass);
         goldText.text = hero.inventory.gold + " Gold";
+        strText.text = "STR: " + hero.strength;
+        dexText.text = "DEX: " + hero.dexterity;
+        wisText.text = "WIS: " + hero.wisdom;
+        if (hero.points > 0)
+            pointsText.text = hero.points + " unspent points";
+        else
+            pointsText.text = "";
     }
 
     public static string GetHeroClassName(HeroClass hc)
