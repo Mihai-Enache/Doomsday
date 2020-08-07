2044945f1ded7cf6d069681c9397fe6be409c85c
Stefan247 <34254467+Stefan247@users.noreply.github.com>
2020-08-07 14:55:17 +0300

    finalBossRoom added

---
+++ rpgDoomsday/Assets/AI_Scripts/FinalBossSpawn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossSpawn : MonoBehaviour
+{
+
+    public GameObject finalBossPrefab;
+    public GameObject towerPrefab;
+    public GameObject helpersPrefab;
+    public GameObject gatePrefab;
+    private Vector3 scaleChange;
+
+    private void Start()
+    {
+        scaleChange = new Vector3(0f, 0f, 2.5f);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        Invoke("CloseGateBoss", 0.5f);
+        Invoke("SpawnFinalBoss", 3.0f);
+        Invoke("SpawnBossTowers", 15f);
+        Invoke("SpawnHarderTower", 30f);
+    }
+
+    void CloseGateBoss()
+    {
+        GameObject gate = Instantiate(gatePrefab, transform.position + new Vector3(0, 0.97f, 0), Quaternion.identity);
+        gate.transform.Rotate(0, 90f, 0);
+        gate.transform.localScale += scaleChange;
+    }
+    
+    void SpawnFinalBoss()
+    {
+        Instantiate(finalBossPrefab, transform.position + new Vector3(0, 1, 40), Quaternion.identity);
+    }
+
+    void SpawnBossTowers()
+    {
+        Instantiate(towerPrefab, transform.position + new Vector3(18, 1, 40), Quaternion.identity);
+        Instantiate(towerPrefab, transform.position + new Vector3(-18, 1, 40), Quaternion.identity);
+        Instantiate(towerPrefab, transform.position + new Vector3(18, 1, 10), Quaternion.identity);
+        Instantiate(towerPrefab, transform.position + new Vector3(-18, 1, 10), Quaternion.identity);
+    }
+
+    void SpawnHarderTower()
+    {
+        Instantiate(towerPrefab, transform.position + new Vector3(0, 1, 27.5f), Quaternion.identity);
+    }
+}
